6f8d51c5f116fbc4f3a492f16c1dd0260166d33d
roger-davila <rogerddavila@gmail.com>
2022-09-14 17:52:09 -0700

    Time: 181 ms (36.15%), Space: 55.7 MB (28.88%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -4,21 +4,16 @@
  */
 var productExceptSelf = function(nums) {
     const results = [];
-    const preFixes = [nums[0]];
-    const postFixes = [nums[nums.length - 1]];
-    // Get product prefixes.
-    for (let i = 1; i < nums.length; i++) {
-        preFixes.push(nums[i] * preFixes[preFixes.length - 1]);
-    }
-    // Get product postfixes.
-    for (let i = nums.length - 2; i >= 0; i--) {
-        postFixes.unshift(nums[i] * postFixes[0]);
-    }
-    // Multiply prefix and postfixs for current number.
+    let prefixProduct = 1;
+    let postfixProduct = 1;
+    
     for (let i = 0; i < nums.length; i++) {
-        const left = preFixes[i - 1] !== undefined ? preFixes[i - 1] : 1;
-        const right = postFixes[i + 1] !== undefined ? postFixes[i + 1] : 1;
-        results.push(left * right);
+        results[i] = prefixProduct;
+        prefixProduct *= nums[i];
+    }
+    for (let j = nums.length - 1; j >= 0; j--) {
+        results[j]*=postfixProduct;
+        postfixProduct *= nums[j];
     }
     return results;
 };
