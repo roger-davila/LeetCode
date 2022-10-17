324e5ae85ce2b7d541a8c45dbb8ffad288e17ad2
roger-davila <rogerddavila@gmail.com>
2022-10-17 16:31:50 -0700

    Time: 74 ms (86.63%), Space: 42.6 MB (65.45%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -3,11 +3,6 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    const alphabet = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
-    
-    for (let i = 0; i < sentence.length; i++) {
-        if (alphabet.has(sentence[i])) alphabet.delete(sentence[i]);
-    }
-    
-    return !(alphabet.size > 0);
+    const sentenceSet = new Set([...sentence]);
+    return sentenceSet.size === 26;
 };
